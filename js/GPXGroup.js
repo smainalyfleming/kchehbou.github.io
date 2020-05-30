04dd79e8bb155432ea1c0ae433595f95d6988774
kchehbou <66142058+kchehbou@users.noreply.github.com>
2020-05-30 11:51:26 -0400

    Update GPXGroup.js

--- js/GPXGroup.js
+++ js/GPXGroup.js
@@ -1,35 +1,4 @@
-L.Mixin.Selectable = {
- includes: L.Mixin.Events,
- setSelected: function(s) {
-  var selected = !!s;
-  if (this._selected !== selected) {
-   this._selected = selected;
-   this.fire("selected")
-  }
- },
- isSelected: function() {
-  return !!this._selected
- }
-};
-L.Mixin.Selection = {
- includes: L.Mixin.Events,
- getSelection: function() {
-  return this._selected
- },
- setSelection: function(item) {
-  if (this._selected === item) {
-   if (item !== null) {
-    item.setSelected(!item.isSelected());
-    if (!item.isSelected()) this._selected = null
-   }
-  } else {
-   if (this._selected) this._selected.setSelected(false);
-   this._selected = item;
-   if (this._selected) this._selected.setSelected(true)
-  }
-  this.fire("selection_changed")
- }
-};
+
 L.Control.LayersLegend = L.Control.Layers.extend({
  _onInputClick: function() {
   var inputs = this._layerControlInputs,
