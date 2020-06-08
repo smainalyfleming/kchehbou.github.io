ba177dd531a46d12f101f67ac0d1fac5f6129520
kchehbou <66142058+kchehbou@users.noreply.github.com>
2020-06-07 21:51:31 -0400

    Update leaflet-gpxgroup.mincolortest.js

--- js/leaflet-gpxgroup.mincolortest.js
+++ js/leaflet-gpxgroup.mincolortest.js
@@ -50,64 +50,60 @@ L.Control.LayersLegend = L.Control.Layers.extend({
  }
 });
 L.control.layersLegend = function(baseLayers, overlays, options) {
- return new L.Control.LayersLegend(baseLayers, overlays, options)
-};
+         return new L.Control.LayersLegend(baseLayers,                overlays, options)
+        };
+
 L.GPX.include(L.Mixin.Selectable);
+
 L.GpxGrroup = L.Class.extend({
- options: {
-  highlight: {
-   color: "#ff0",
-   opacity: 1
-  },
-  points: [],
-  points_options: {
-   icon: {
-    iconUrl: "../images/elevation-poi.png",
-    iconSize: [12, 12]
-   }
-  },
-  polygons: [],
-  polygons_options: {
-    color: "grey",
-  },
-  flyToBounds: true,
-  legend: false,
-  legend_options: {
-   position: "topright",
-   collapsed: false
-  },
-  elevation: true,
-  elevation_options: {
-   theme: "yellow-theme",
-   detachedView: true,
-   elevationDiv: "#elevation-div"
-  },
-  distanceMarkers: true,
-  distanceMarkers_options: {
-   lazy: true
-  }
- },
- initialize: function(tracks, options) {
-  L.Util.setOptions(this, options);
-  this._count = 0;
-  this._loadedCount =
-   0;
-  this._tracks = tracks;
-  this._routes = {};
-  this._layers = L.featureGroup();
-  this._markers = L.featureGroup();
-  this._elevation = L.control.elevation(this.options.elevation_options);
-  this._legend = L.control.layersLegend(null, null, this.options.legend_options);
-  var icon = L.icon(this.options.points_options.icon);
-  this.options.points.forEach(function(poi) {
-   var marker = L.marker(poi.latlng, {
-    icon: icon
-   });
-   marker.bindTooltip(poi.name, {
-    direction: "auto"
-   });
-   marker.addTo(this._markers)
-  }, this);
+      options: {
+        /*options for the legend */
+          highlight: {
+          color: "#ff0",
+          opacity: 1,
+            weight: 5
+                    },
+          highlight1: {
+          color: "grey",
+          opacity: 1,
+            weight:2
+                    },
+          points: [],
+  
+          polygons: [],
+          polygons_options: {
+               color: "grey",
+                            },
+         flyToBounds: true,
+          legend: false,
+          legend_options: {
+              position: "topright",
+              collapsed: false
+                          },
+          elevation: true,
+          elevation_options: {
+              theme: "yellow-theme",
+              detachedView: true,
+              elevationDiv: "#elevation-div"
+              
+                              },
+          distanceMarkers: true,
+          distanceMarkers_options: {
+              lazy: true
+                                    }
+                },
+      /*end options for the legend */
+      initialize: function(tracks, options) {
+          L.Util.setOptions(this, options);
+          this._count = 0;
+          this._loadedCount =0;
+          this._tracks = tracks;
+          this._routes = {};
+          this._layers = L.featureGroup();
+          this._markers = L.featureGroup();
+          this._elevation =                                            L.control.elevation(this.options.elevation_options);
+          this._legend = L.control.layersLegend(null, null,                   this.options.legend_options);
+          
   if (typeof L.DistanceMarkers === "undefined") this.options.distanceMarkers =
    false
  },
@@ -125,36 +121,58 @@ L.GpxGrroup = L.Class.extend({
  addTrack: function(track) {
   this._get(track, this._loadRoute.bind(this))
  },
+  
  _loadRoute: function(data) {
-  var colors = this._uniqueColors(this._tracks.length);
-  var color = colors[this._count++];
-  var line_style = {
-   color: color,
-   opacity: .75,
-   weight: 3,
-   distanceMarkers: this.options.distanceMarkers_options
-  };
+    var colors = this._uniqueColors(this._tracks.length);
+    var color = colors[this._count++];
+    var line_style = {
+      color: color,
+      opacity: .75,
+      weight:2,
+      distanceMarkers: this.options.distanceMarkers_options
+                      };
+   var line_style1 = {
+      color: color,
+      weight:2,
+      opacity: .75,
+      distanceMarkers: this.options.distanceMarkers_options
+                    };
+   var line_style3 = {
+      color: 'grey',
+      weight:2,
+                    };
+   
   var marker_style = {
-   startIconUrl: null,
-   endIconUrl: null
-  };
+      startIconUrl: null,
+      endIconUrl: null
+                    };   
   var route = new L.GPX(data, {
-   async: true,
-   marker_options: marker_style,
-   polyline_options: line_style
-  });
+      async: true,
+      marker_options: marker_style,
+      polyline_options: line_style1,
+                        });
   route.originalStyle = line_style;
-  route.on("addline", L.bind(this._onRouteAddLine, this, route));
-  route.on("loaded", L.bind(this._onRouteLoaded, this, route));
+  route.originalStyle1 = line_style3;
+  route.on("addline", L.bind(this._onRouteAddLine, this,            route),);
+  route.on("loaded", L.bind(this._onRouteLoaded, this,              route));
   route.addTo(this._layers);
   this._routes[route._leaflet_id] = route
- },
- _onRouteAddLine: function(route, e) {
-  var polyline = e.line;
-  route.on("selected",
-   L.bind(this._onRouteSelected, this, route, polyline));
-  polyline.on("mouseover", L.bind(this._onRouteMouseOver, this, route, polyline));
-  polyline.on("mouseout", L.bind(this._onRouteMouseOut, this, route, polyline));
+},
+  
+_onRouteAddLine: function(route, e) {
+   var polyline = e.line;
+   route.on("selected",
+   L.bind(this._onRouteSelected, this, route,                        polyline),polyline.setStyle({
+          weight: 2
+            }));
+  
+   polyline.on("mouseover", L.bind(this._onRouteMouseOver,          this, route, polyline)/*Kamel: Reinitialize Trails width after mouse out */,polyline.setStyle({
+          weight: 2
+}));
+   polyline.on("mouseout", L.bind(this._onRouteMouseOut,            this, route, polyline)/*Kamel: Reinitialize Trails width after mouse out */,polyline.setStyle({
+          weight: 2
+     
+}));
   polyline.on("click", /*L.bind(this._onRouteClick, this, route, polyline),*/ function (event) {
     // Assuming the clicked feature is a shape, not a point marker.
     map.fitBounds(event.layer.getBounds());
@@ -163,15 +181,15 @@ L.GpxGrroup = L.Class.extend({
    direction: "auto",
    sticky: true
   })
- },
- highlight: function(route, polyline) {
+ }, 
+  highlight: function(route, polyline) {
+  polyline.setStyle(route.originalStyle1);
   polyline.setStyle(this.options.highlight);
-  if (this.options.distanceMarkers) polyline.addDistanceMarkers()
  },
+  
  unhighlight: function(route,
   polyline) {
-  polyline.setStyle(route.originalStyle);
-  if (this.options.distanceMarkers) polyline.removeDistanceMarkers()
+  polyline.setStyle(route.originalStyle1);
  },
  _onRouteMouseOver: function(route, polyline) {
   if (!route.isSelected()) {
@@ -183,14 +201,17 @@ L.GpxGrroup = L.Class.extend({
   }
   this.fire("route_mouseover", {
    route: route,
-   polyline: polyline
-  })
+   polyline: polyline,
+function(route, polyline) {
+  polyline.setStyle(route.originalStyle1);
+  polyline.setStyle(this.options.highlight);
+ },  })
  },
  _onRouteMouseOut: function(route, polyline) {
   if (!route.isSelected()) this.unhighlight(route, polyline);
   this.fire("route_mouseout", {
    route: route,
-   polyline: polyline
+   polyline: polyline,
   })
  },
  _onRouteClick: function(route, polyline) {
@@ -200,35 +221,34 @@ L.GpxGrroup = L.Class.extend({
   if (!route.isSelected()) this.unhighlight(route, polyline)
  },
  _onRouteLoaded: function(route) {
-  if (this.options.legend) this._legend.addBaseLayer(route, '<svg id="legend_' + route._leaflet_id + '" width="25" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">' + '<line x1="0" x2="50" y1="5" y2="5" stroke="' + route.options.polyline_options.color +
-   '" fill="grey" stroke-width="5" /></svg>' + " " + route.get_name());
-  this.fire("route_loaded", {
-   route: route
-  });
-  if (++this._loadedCount === this._tracks.length) {
-   this.fire("loaded");
-   if (this.options.flyToBounds) this._map.flyToBounds(this.getBounds(), {
-    duration: .25,
-    easeLinearity: .25,
-    noMoveStart: true
-   });
-   if (this.options.legend) this._legend.addTo(this._map)
-  }
+     if (this.options.legend)                                         this._legend.addBaseLayer(route, '<svg id="legend_' +         route._leaflet_id + '" width="25" height="10"                 version="1.1" xmlns="http://www.w3.org/2000/svg">' +         '<line x1="5" x2="10" y1="10" y2="3" stroke="' +             route.options.polyline_options.color + '    "                 fill="yellow" stroke-width="4" /></svg>' +                   route.get_name());
+            this.fire("route_loaded", {
+                route: route
+                      });
+    if (++this._loadedCount === this._tracks.length) {
+        this.fire("loaded");
+        if (this.options.flyToBounds)                                     this._map.flyToBounds(this.getBounds(), {
+            duration: .25,
+            easeLinearity: .25,
+            noMoveStart: true
+                                                });
+        if (this.options.legend)                                        this._legend.addTo(this._map)
+                                                    }
  },
  _onSelectionChanged: function(e) {
-  var elevation = this._elevation;
-  var eleDiv = elevation.getContainer();
-  var route = this.getSelection();
-  elevation.clear();
-  if (route &&
-   route.isSelected()) {
-   if (!eleDiv) elevation.loadChart(this._map);
-   route.getLayers().forEach(function(layer) {
-    if (layer instanceof L.Polyline) {
-     elevation.addData(layer);
+    var elevation = this._elevation;
+    var eleDiv = elevation.getContainer();
+    var route = this.getSelection();
+    elevation.clear();
+    if (route &&
+      route.isSelected()) {
+          if (!eleDiv) elevation.loadChart(this._map);
+              route.getLayers().forEach(function(layer) {
+                if (layer instanceof L.Polyline) {
+                    elevation.addData(layer);
     /* layer.bringToFront()*/
-    }
-   })
+                                                  }
+                                                        })
   } else if (eleDiv) elevation.remove()
  },
  _onLegendSelected: function(e) {
@@ -318,9 +338,7 @@ L.GpxGrroup = L.Class.extend({
  _byteToHex: function(n) {
   return (n >> 4 & 15).toString(16) + (n & 15).toString(16)
  },
- removeFrom: function(map) {
-  this._layers.removeFrom(map)
- }
+
 });
 L.GpxGrroup.include(L.Mixin.Events);
 L.GpxGrroup.include(L.Mixin.Selection);
